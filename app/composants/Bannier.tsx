5de89ccbe3862a38b6df0273b90236cb46abceaf
SKB-TECH <shakokinyamba201@gmail.com>
2024-10-29 17:18:27 +0100

    add all

---
+++ app/composants/Bannier.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Bannier = () => {
+  return (
+    <div>Bannier</div>
+  )
+}
+
+export default Bannier
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <section className='w-full h-full bg-red-500'> 
-      page
+        Je suis benjamin shako
     </section>
   )
 }
